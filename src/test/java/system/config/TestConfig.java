13ca640902ed48d9c38405d104758833852bd2be
VladMakhov <makhov1@list.ru>
2023-11-12 10:37:49 +0300

    Postgres migration

--- src/test/java/system/config/TestConfig.java
+++ src/test/java/system/config/TestConfig.java
@@ -3,15 +3,15 @@ package system.config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @TestConfiguration
 public class TestConfig {
 
     @Bean
     @ServiceConnection
-    public MySQLContainer<?> mySQLContainer() {
-        return new MySQLContainer<>("mysql:latest");
+    public PostgreSQLContainer<?> mySQLContainer() {
+        return new PostgreSQLContainer<>("postgres:latest");
     }
 
 }
